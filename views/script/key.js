e17ee9541b31bd06094a6c0264ee6c11abb1027c
semorang <semorang@gmail.com>
2023-05-31 14:16:21 +0900

    add dist api key

--- views/script/key.js
+++ views/script/key.js
@@ -2,6 +2,12 @@ const KAKAOMAPAPIKEY='9def7442ad15b4775780f565ab8cd9c4'; // 개인적으로 카�
 // const INAVIMAPAPIKEY='IWbzfwiPMHdIlSDU'; // 재일씨가 재공해준 값, 잠깐만 쓰고 새로 받는 해야함.(22-11-30)
 const INAVIMAPAPIKEY='3cbfd55815743b0a8cfb7d4dc6a6d11331570d2d'; // 2023-01-13 제공 받음
 
+// distance matrix api key
+const DISTMATRIXAPIKEYS = [
+    {name: 'rpteam', apiKey: '1PAWS6Y-5Z74SJ3-P613MCC-M24VCX0', uuid: '0d95cc9b-2fce-4cc8-b182-3a31a089b674'}, // for inavi rp team
+    {name: 'fleetune', apiKey: '035VE7J-2EF4T7W-J9PZ6GB-9H8SVHC', uuid: '00cbb71e-139e-4d1f-926d-f3414c519dc5'}, // for fleetune
+];
+
 module.exports = {
     KAKAOMAPAPIKEY,
     INAVIMAPAPIKEY,
